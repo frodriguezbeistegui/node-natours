d433f84c98de7e36391415abdc1b585411e94545
facundo <frodriguezbeistegui@gmail.com>
2022-06-02 20:23:23 -0300

    mongoDB and mongoose implemented

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 //  1) MIDDLEWARE
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello form the Middleware! 👋🏼');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello form the Middleware! 👋🏼');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -23,6 +26,5 @@ app.use((req, res, next) => {
 //3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    console.log('DB connection succesfull');
+  });
+
+const app = require('./app');
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
-
