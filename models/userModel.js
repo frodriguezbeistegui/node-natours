6970b1daa7e93c3fbf01aabb87a37aa2ec8fe6c4
facundo <frodriguezbeistegui@gmail.com>
2022-06-13 21:51:32 -0300

    login added

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,29 +20,34 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: [8, 'A tour password must have more o equal than 8 characters'],
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
       // this will only works on CREATE and SAVE!!!
-      validator: function(el){
-        return el === this.password
+      validator: function (el) {
+        return el === this.password;
       },
-      message: 'Passwords are not the same'
-    }
+      message: 'Passwords are not the same',
+    },
   },
 });
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   // Only run this function if password was actually modified
-  if(!this.isModified('password')) return next()
+  if (!this.isModified('password')) return next();
 
   // Hash the password with cost of 12
-  this.password = await bcrypt.hash(this.password, 12)
+  this.password = await bcrypt.hash(this.password, 12);
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
-})
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+};
 
 const User = mongoose.model('User', userSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
@@ -29,9 +31,9 @@ mongoose
   })
   .then(() => console.log('DB connection succesfull'));
 
-const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
