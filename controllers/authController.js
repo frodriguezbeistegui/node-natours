525f3095c29ebdb2b4079e0960e6dbaa6f645dae
facundo <frodriguezbeistegui@gmail.com>
2022-07-23 10:21:06 -0300

    updating user photo feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -118,7 +118,6 @@ exports.protect = catchAsync(async (req, res, next) => {
   res.locals.user = currentUser;
   next();
 });
-
 // Only for rendered pages, no errors
 exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
@@ -137,7 +136,6 @@ exports.isLoggedIn = async (req, res, next) => {
       if (currentUser.changedPasswordAfter(decoded.iat)) {
         return next();
       }
-
       // There is a logged in user
       res.locals.user = currentUser;
       return next();
