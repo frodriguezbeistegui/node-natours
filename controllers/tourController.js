52614ca0f02ff979323bed63c701bbfe63b589c7
facundo <frodriguezbeistegui@gmail.com>
2022-07-24 02:23:52 -0300

    updating tour images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,35 @@
+const sharp = require('sharp');
+const multer = require('multer');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  {name: 'imageCover', maxCount: 1},
+  {name: 'images', maxCount: 3}
+])
+// upload.array('images', 5) for multiple images in the same field
+// Resizing images for the tour array
+exports.resizeTourImages = (req,res,next) => {
+  console.log(req.files)
+  next()
+}
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
