a2ab0b83671a3d1ba3f0653e022d56be98fc3542
facundo <frodriguezbeistegui@gmail.com>
2022-07-25 11:32:03 -0300

    advanced email handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,15 +21,40 @@ const upload = multer({
 });
 
 exports.uploadTourImages = upload.fields([
-  {name: 'imageCover', maxCount: 1},
-  {name: 'images', maxCount: 3}
-])
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
 // upload.array('images', 5) for multiple images in the same field
 // Resizing images for the tour array
-exports.resizeTourImages = (req,res,next) => {
-  console.log(req.files)
-  next()
-}
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  console.log(req.files);
+
+  if (!req.files.imageCover || !req.files.images) return next();
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+  //  2) IMAGES
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+  next();
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,53 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug')
+const htmlToText = require('html-to-text')
+// new Email(user, url).sendWelcome()
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.fisrtName = user.name.split(' ')[0]
+    this.url = url,
+    this.from = `Facundo Rodrigruez <${process.env.EMAIL_FROM}>`
+  }
+  
+  newTransport(){
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return 1
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      // service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      // Activate in gmail "less secure app" option
+    });
+  }
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    // service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activeate in gmail "less secure app" option
-  });
-  // 2) Define the email options
-  const mailOptions = {
-    form: 'Facundo Rodrigruez <test@email.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
-  //   3) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
+  async send(template, subject){
+    // Send the actual email
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/..views/emails/${template}.pug`, {
+      fisrtName: this.fisrtName,
+      url: this.url,
+      subject
+    })
+    // 2) Define email options
+    const mailOptions = {
+      form: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions)
+  }
 
-module.exports = sendEmail;
+  async sendWelcome(){
+    await this.send('welcome', 'Welcome to the Natours Family!')
+  }
+}
